a6e6d7bc218492b24e549370f0674aeb1ced5758
Daniel O'Grady <danielogrady@gmx.de>
2017-08-13 21:37:19 +0200

    Add setup script

---
+++ setup.py
@@ -0,0 +1,71 @@
+""" Ambient wakeup script
+
+See:
+https://github.com/ogrady/GoodMorning
+"""
+
+# Always prefer setuptools over distutils
+from setuptools import setup, find_packages
+# To use a consistent encoding
+from codecs import open
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+
+with open(path.join(here, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name='GoodMorning',
+    version='1.0.0',
+
+    description='Ambient wakeup script',
+    long_description='',
+
+    # The project's main homepage.
+    url='https://github.com/ogrady/GoodMorning',
+
+    # Author details
+    author='Daniel O\'Grady',
+    author_email='danielogrady@gmx.de',
+
+    # Choose your license
+    license='GPL-3.0',
+
+    # See https://pypi.python.org/pypi?%3Aaction=list_classifiers
+    classifiers=[
+        # How mature is this project? Common values are
+        #   3 - Alpha
+        #   4 - Beta
+        #   5 - Production/Stable
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Topic :: Ambient App',
+        'License :: OSI Approved :: GPL-3.0',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+    ],
+
+    keywords='ambient',
+
+    # You can just specify the packages manually here if your project is
+    # simple. Or you can use find_packages().
+    #packages=find_packages(exclude=['contrib', 'docs', 'tests*']),
+
+    # List run-time dependencies here.  These will be installed by pip when
+    # your project is installed. For an analysis of "install_requires" vs pip's
+    # requirements files see:
+    # https://packaging.python.org/en/latest/requirements.html
+    install_requires=['pygame']
+
+    # List additional groups of dependencies here (e.g. development
+    # dependencies). You can install these using the following syntax,
+    # for example:
+    # $ pip install -e .[dev,test]
+    #extras_require={
+    #    'dev': ['check-manifest'],
+    #    'test': ['coverage'],
+    #},
+)
