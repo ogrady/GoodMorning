37533a3d4f2bc37bdcebb501b55ec310f11fd9f6
Daniel O'Grady <danielogrady@gmx.de>
2017-08-20 20:19:29 +0200

    Add schedule to dependencies

--- setup.py
+++ setup.py
@@ -58,7 +58,7 @@ setup(
     # your project is installed. For an analysis of "install_requires" vs pip's
     # requirements files see:
     # https://packaging.python.org/en/latest/requirements.html
-    install_requires=['pygame']
+    install_requires=['pygame', 'schedule']
 
     # List additional groups of dependencies here (e.g. development
     # dependencies). You can install these using the following syntax,
