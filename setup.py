273c21f7f8a04f41b362690f19aa42095078a18d
Daniel O'Grady <danielogrady@gmx.de>
2017-09-02 16:31:32 +0200

    Add depdencies for LED strip to setup.py

--- setup.py
+++ setup.py
@@ -58,7 +58,7 @@ setup(
     # your project is installed. For an analysis of "install_requires" vs pip's
     # requirements files see:
     # https://packaging.python.org/en/latest/requirements.html
-    install_requires=['pygame', 'schedule']
+    install_requires=['pygame', 'schedule', 'RPi.GPIO', 'adafruit-ws2801']
 
     # List additional groups of dependencies here (e.g. development
     # dependencies). You can install these using the following syntax,
